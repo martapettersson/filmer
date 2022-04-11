ff40af8ccd7086b9a138cb5d4d3189bd38df1733
Marta Pettersson <marta.pettersson@gmail.com>
2022-04-11 15:02:08 +0200

    move Loader to App.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,27 +1,38 @@
 import React, { useState } from "react";
 import Headline from "./components/Headline";
+import Loader from "./components/Loader";
 import DownloadPage from "./pages/DownloadPage";
 import MoviePage from "./pages/MoviePage";
 import { getMovies } from "./utils/fetch";
 
 export default function App() {
+	const [showRes, setShowRes] = useState(false);
+	const [downloadReq, setDowloadReq] = useState(false);
 	const [moviePages, setMoviePages] = useState(null);
 	const pages = [1, 2, 3, 4, 5]; // 20 movies/page, starting with page 1
+
 	async function downloadMovies() {
+		setDowloadReq(true);
 		let data = [];
 		for (let page of pages) {
 			let res = await getMovies(page);
 			data.push(res);
 		}
 		setMoviePages(data);
+		setTimeout(() => {
+			setShowRes(true);
+		}, 2500);
 	}
+
 	return (
 		<div className="container">
 			<Headline title="Toppfilmer just nu" size={1} />
-			{moviePages ? (
-				<MoviePage moviePages={moviePages} />
-			) : (
+			{!downloadReq ? (
 				<DownloadPage downloadMovies={downloadMovies} />
+			) : !showRes || !moviePages ? (
+				<Loader />
+			) : (
+				<MoviePage moviePages={moviePages} />
 			)}
 		</div>
 	);
--- src/app/pages/MoviePage.jsx
+++ src/app/pages/MoviePage.jsx
@@ -1,18 +1,10 @@
 import React, { useEffect, useState } from "react";
-import Loader from "../components/Loader";
 import OrderedList from "../components/OrderedList";
 
 export default function MoviePage({ moviePages }) {
-	const [showRes, setShowRes] = useState(false);
 	let movies = [];
 	for (let page of moviePages) {
 		page.results?.map((res) => movies.push(res));
 	}
-
-	useEffect(() => {
-		setTimeout(() => {
-			setShowRes(true);
-		}, 2500);
-	});
-	return <>{!showRes ? <Loader /> : <OrderedList movies={movies} />}</>;
+	return <>{<OrderedList movies={movies} />}</>;
 }
