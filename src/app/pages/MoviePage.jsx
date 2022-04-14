4f01ecac0dbde3d535416b086e80721672f11d38
Marta Pettersson <marta.pettersson@gmail.com>
2022-04-14 11:02:57 +0200

    add error msg to movie page

--- src/app/pages/MoviePage.jsx
+++ src/app/pages/MoviePage.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import OrderedList from "../components/OrderedList";
+import styled from "styled-components";
+
+const Error = styled.div`
+	font-size: 18px;
+	line-height: 20px;
+`;
 
 export default function MoviePage({ moviePages }) {
 	let movies = [];
 	for (let page of moviePages) {
 		page.results?.map((res) => movies.push(res));
 	}
-	return <>{<OrderedList movies={movies} />}</>;
+	return (
+		<>
+			{movies.length > 0 ? (
+				<OrderedList movies={movies} />
+			) : (
+				<Error>something went wrong...</Error>
+			)}
+		</>
+	);
 }
